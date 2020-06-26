75c2644d1f34b4101b3fec95ed13ee5014e38a9c
Arthur Diniz <arthurbdiniz@gmail.com>
2020-06-26 13:40:02 -0300

    Add lambda permissions to cloudwatch logs
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- _variables.tf
+++ _variables.tf
@@ -5,3 +5,7 @@ variable "bucket_name" {
 variable "elasticsearch_domain_name" {
   default = "cloud-watch"
 }
+
+variable "firehose_lambda_processor_name" {
+  default = "firehose_lambda_processor"
+}
--- lambda-processor.tf
+++ lambda-processor.tf
@@ -1,12 +1,13 @@
 resource "aws_lambda_function" "lambda_processor" {
-  filename      = "firehose_lambda_processor.zip"
-  function_name = "firehose_lambda_processor"
-  role          = aws_iam_role.lambda_iam.arn
-  description   = "An Amazon Kinesis Firehose stream processor that extracts individual log events from records sent by Cloudwatch Logs subscription filters."
-  handler       = "lambda_function.handler"
-  runtime       = "python2.7"
+  filename         = "firehose_lambda_processor.zip"
+  function_name    = var.firehose_lambda_processor_name
+  role             = aws_iam_role.lambda_iam.arn
+  description      = "An Amazon Kinesis Firehose stream processor that extracts individual log events from records sent by Cloudwatch Logs subscription filters."
+  handler          = "lambda_function.handler"
+  runtime          = "python2.7"
   source_code_hash = data.archive_file.lambda_processor_zip.output_base64sha256
-  timeout       = 180
+  timeout          = 180
+  depends_on       = [aws_iam_role_policy_attachment.lambda_logs, aws_cloudwatch_log_group.example]
 }
 
 resource "aws_iam_role" "lambda_iam" {
@@ -16,13 +17,44 @@ resource "aws_iam_role" "lambda_iam" {
     "Version": "2012-10-17",
     "Statement": [
         {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-            "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
+          "Action": "sts:AssumeRole",
+          "Principal": {
+              "Service": "lambda.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
         }
     ]
   })
+}
+
+resource "aws_cloudwatch_log_group" "example" {
+  name              = "/aws/lambda/${var.firehose_lambda_processor_name}"
+  retention_in_days = 14
+}
+
+resource "aws_iam_policy" "lambda_processor_logging" {
+  name        = "lambda_processor_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource": "arn:aws:logs:*:*:*",
+        "Effect": "Allow"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_iam.name
+  policy_arn = aws_iam_policy.lambda_processor_logging.arn
 }
