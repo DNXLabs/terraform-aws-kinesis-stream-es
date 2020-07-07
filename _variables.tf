bdde4b6e4c6beaa9fdea969f78511c70a6f2615f
Arthur Diniz <arthurbdiniz@gmail.com>
2020-07-06 21:27:08 -0300

    Give type to variables
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- _variables.tf
+++ _variables.tf
@@ -1,11 +1,15 @@
-variable "elasticsearch_domain_name" {}
+variable "elasticsearch_domain_name" {
+  type    = string
+}
 
 variable "elasticsearch_volume_type" {
   default = "gp2"
+  type    = string
 }
 
 variable "elasticsearch_volume_size" {
   default = 10
+  type    = number
 }
 
 variable "private_subnet_ids" {
@@ -14,12 +18,17 @@ variable "private_subnet_ids" {
 
 variable "firehose_lambda_processor_name" {
   default = "firehose_lambda_processor"
+  type    = string
 }
 
 # variable "kinesis_firehose_index_name" {
 #   default = "kinesis"
 # }
 
-variable "kinesis_stream_bucket_name" {}
+variable "kinesis_stream_bucket_name" {
+  type    = string
+}
 
-variable "vpc_id" {}
+variable "vpc_id" {
+  type    = string
+}
