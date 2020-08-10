2d4364899a3863ff5c8ba83ebe4a01dd51d7b117
Arthur Diniz <arthurbdiniz@gmail.com>
2020-08-10 15:28:15 -0300

    Adds description to output kinesis_arn
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- _output.tf
+++ _output.tf
@@ -1,3 +1,4 @@
 output "kinesis_arn" {
-  value = "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.kinesis_firehose_name}"
+  description = "The ARN of the kinesis firehose stream to be used in the cloudwatch log subscription filter."
+  value       = "arn:aws:firehose:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:deliverystream/${var.kinesis_firehose_name}"
 }
